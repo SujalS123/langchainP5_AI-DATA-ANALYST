45584e283d65884aab0b2b548c9de32d5284e208
SujalS123 <202301040131@mitaoe.ac.in>
2025-10-22 16:05:26 +0530

    frontend conncection issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,6 +17,7 @@ app.add_middleware(
     allow_credentials=True,  # Allow credentials
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
+    expose_headers=["*"]
 )
 
 @app.get("/docs", include_in_schema=False)
